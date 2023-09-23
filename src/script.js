4680813d8321a01238314dbb58bdbaa090354a3b
taitoadachi <taitoadachiFormyself@USERnoMacBook-Pro-2.local>
2023-09-23 19:33:55 +0900

    changed matcap

--- src/script.js
+++ src/script.js
@@ -24,7 +24,7 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
-const matcapTexture = textureLoader.load('/textures/matcaps/3.png')
+const matcapTexture = textureLoader.load('/textures/matcaps/8.png')
 
 /**
  * Fonts
